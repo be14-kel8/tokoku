19d4eb90d25b9b535415be8c1d207bc49ed3b593
Thomas Gianto <thomasgianto@gmail.com>
2022-12-19 23:52:08 +0700

    add code employee

--- employee/employee.go
+++ employee/employee.go
@@ -1,6 +1,9 @@
 package employee
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+)
 
 type Employee struct {
 	id       int
@@ -39,7 +42,7 @@ func (e *Employee) GetUsername() string {
 func (e *Employee) GetName() string {
 	return e.name
 }
-func (e *Employee) GePassword() string {
+func (e *Employee) GetPassword() string {
 	return e.password
 }
 
@@ -47,3 +50,41 @@ type EmployeeAuth struct {
 	DB *sql.DB
 }
 
+func (em *EmployeeAuth) Duplicate(userName string) bool {
+	res := em.DB.QueryRow("SELECT id_employee FROM employees where username = ?", userName)
+
+	tmp := 0
+	err := res.Scan(&tmp)
+	if err != nil {
+		return false
+	}
+	return true
+
+}
+
+func (em *EmployeeAuth) RegisterEmp(newEmp Employee) (bool, error) {
+	registerQry, err := em.DB.Prepare("INSERT INTO employees (username,name,password) values (?,?,?)")
+	if err != nil {
+		return false, errors.New("Column not match")
+	}
+	//duplicate
+	if em.Duplicate(newEmp.GetUsername()) {
+		return false, errors.New("username already exist")
+	}
+
+	//execute
+	res, err := registerQry.Exec(newEmp.GetUsername(), newEmp.GetName(), newEmp.GetPassword())
+	if err != nil {
+		return false, errors.New("Insert Employees Errors")
+	}
+
+	// Chek rows affected
+	affectedRows, err := res.RowsAffected()
+	if err != nil {
+		return false, errors.New("Error after insert")
+	}
+	if affectedRows <= 0 {
+		return false, errors.New("0 affected rows")
+	}
+	return true, nil
+}
